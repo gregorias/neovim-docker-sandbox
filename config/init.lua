8f78bc5a1bef1d5b0ecfde453859795c316549d4
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-04-02 11:38:20 +0200

    refactor: delete lua/utils.lua
    The additonal file was an unnecessary complication. It made it harder to
    report minimal configs in tickets.

--- config/init.lua
+++ config/init.lua
@@ -1,5 +1,19 @@
-require'utils'
-utils.bootstrap_lazy()
+-- | Fetches Lazy if not present.
+local function bootstrap_lazy()
+  local lazypath = vim.fn.stdpath('data')..'/lazy/lazy.nvim'
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system{
+      'git', 'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable',
+      lazypath,
+    }
+  end
+  vim.opt.rtp:prepend(lazypath)
+end
+bootstrap_lazy()
+
 require'lazy'.setup({
   { 'nvim-treesitter/nvim-treesitter',
     build = function()
